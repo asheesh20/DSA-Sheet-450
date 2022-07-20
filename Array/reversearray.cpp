dbafaa3ebd41b1bb47c70c84103855195cad5cfc
Asheesh Kumar <asheesh220720@gmail.com>
2022-07-20 19:06:21 +0530

    Update reversearray.cpp

--- Array/reversearray.cpp
+++ Array/reversearray.cpp
@@ -3,6 +3,7 @@
 Link : https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
 
 */
+// USING ARRAY :
 
 #include<bits/stdc++.h>
 #define ll long long 
@@ -36,4 +37,34 @@ void print(int a[],int n){
 /*
 TC : O(N)
 SC : O(1)
+*/
 
+// USING VECTOR :
+
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+vector<int> reverse(vector<int> v);
+void print(vector<int> v);
+int32_t main(){
+	vector<int> v;
+	v.push_back(11);
+	v.push_back(1);
+	v.push_back(-9);
+	v.push_back(0);
+	vector<int> ans=reverse(v);
+	print(ans);
+}
+vector<int> reverse(vector<int> v){
+	int start=0,end=v.size()-1;
+	while(start<end){
+		swap(v[start],v[end]);
+		start++;end--;
+	}
+	return v;
+}
+void print(vector<int> v){
+	for(int i=0;i<v.size();i++){
+		cout<<v[i]<<" ";
+	}
+}
